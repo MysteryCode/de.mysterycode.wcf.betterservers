7e363fa0c07bdb1a9ee3b1803d246744a5015615
mutec <mysterycode@mysterycode.de>
2021-03-16 12:40:57 +0100

    ease up things

--- files/lib/data/package/update/BetterServersPackageUpdateAction.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace wcf\data\package\update;
-
-class BetterServersPackageUpdateAction extends PackageUpdateAction {
-
-}
--- files/lib/system/event/listener/BetterServersWoltLabServerListListener.class.php
+++ files/lib/system/event/listener/BetterServersWoltLabServerListListener.class.php
@@ -2,11 +2,6 @@
 
 namespace wcf\system\event\listener;
 
-use wcf\acp\page\PackageUpdateServerListPage;
-use wcf\data\package\update\server\DecoratedToggleablePackageUpdateServer;
-use wcf\data\package\update\server\PackageUpdateServerList;
-use wcf\data\package\update\server\ToggleablePackageUpdateServer;
-use wcf\data\package\update\server\ToggleablePackageUpdateServerList;
 use wcf\system\WCFACP;
 
 class BetterServersWoltLabServerListListener implements IParameterizedEventListener {
@@ -14,25 +9,8 @@ class BetterServersWoltLabServerListListener implements IParameterizedEventListe
 	 * @inheritDoc
 	 */
 	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (!PACKAGE_SERVER_BETTERSERVERS) return;
-		
-		if ($eventObj instanceof PackageUpdateServerListPage) {
-			return;
-			if ($eventName == 'readParameters') {
-				$eventObj->objectListClassName = ToggleablePackageUpdateServerList::class;
-				$eventObj->templateName = 'betterServersPackageUpdateServerList';
-				$eventObj->templateNameApplication = 'wcf';
-			}
-		}
-		else if ($eventObj instanceof PackageUpdateServerList && !($eventObj instanceof ToggleablePackageUpdateServerList)) {
-			return;
-			$eventObj->className = ToggleablePackageUpdateServer::class;
-			$eventObj->decoratorClassName = DecoratedToggleablePackageUpdateServer::class;
-		}
-		else if ($eventObj instanceof WCFACP) {
-			if ($eventName == 'initialized') {
-				@include_once(WCF_DIR . '/lib/data/package/update/server/BetterServersPackageUpdateServer.class.php');
-			}
+		if (PACKAGE_SERVER_BETTERSERVERS && $eventObj instanceof WCFACP && $eventName == 'initialized') {
+			@include_once(WCF_DIR . '/lib/data/package/update/server/BetterServersPackageUpdateServer.class.php');
 		}
 	}
 }
