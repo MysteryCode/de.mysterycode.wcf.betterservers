2ee80ddf0ca2497a0fe791693006f4815cb7a92d
mutec <mysterycode@mysterycode.de>
2022-07-29 14:36:54 +0200

    WSC 5.5

--- files/lib/system/event/listener/BetterServersWoltLabServerListListener.class.php
+++ files/lib/system/event/listener/BetterServersWoltLabServerListListener.class.php
@@ -4,13 +4,12 @@ namespace wcf\system\event\listener;
 
 use wcf\system\WCFACP;
 
-class BetterServersWoltLabServerListListener implements IParameterizedEventListener {
-	/**
-	 * @inheritDoc
-	 */
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (PACKAGE_SERVER_BETTERSERVERS && $eventObj instanceof WCFACP && $eventName == 'initialized') {
-			@include_once(WCF_DIR . '/lib/data/package/update/server/BetterServersPackageUpdateServer.class.php');
-		}
-	}
+class BetterServersWoltLabServerListListener extends AbstractEventListener
+{
+    protected function onInitialized(WCFACP $eventObj)
+    {
+        if (PACKAGE_SERVER_BETTERSERVERS && $eventName == 'initialized') {
+            @include_once(WCF_DIR . '/lib/data/package/update/server/BetterServersPackageUpdateServer.class.php');
+        }
+    }
 }
